057d522acbc2d6ce40b6b690f1f91157ba82084d
JiachuanXu <jiachuanxu@email.arizona.edu>
2025-02-13 14:40:56 -0500

    Add new multiprobe combinations

---
+++ likelihood/desy3xplanck_gk3x2pt.py
@@ -0,0 +1,11 @@
+from cobaya.likelihoods.desy1xplanck._cosmolike_prototype_base import _cosmolike_prototype_base
+import cosmolike_desy1xplanck_interface as ci
+import numpy as np
+
+class desy3xplanck_gk3x2pt(_cosmolike_prototype_base):
+  # ------------------------------------------------------------------------
+  # ------------------------------------------------------------------------
+  # ------------------------------------------------------------------------
+
+  def initialize(self):
+    super(desy3xplanck_gk3x2pt,self).initialize(probe="gk3x2pt")
